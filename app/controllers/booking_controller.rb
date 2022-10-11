4c6385d7adc146c05e20fcd0a3c0ed3f07e3e08a
Nuzhah Kureemun <nuzk2904@gmail.com>
2022-10-11 11:38:57 +0400

    still working on controller booking

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,23 @@
+class BookingController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @booking.boat = @boat
+    if @booking.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    # TO DO
+  end
+end
