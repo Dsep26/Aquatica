18539d5f8cba553029e7406b68055d0922d1199e
Darshan Seetaram <darshanseetaram@gmail.com>
2022-10-14 14:20:25 +0400

    add validation and edit-new-booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :boat
   belongs_to :user
+  validates :start_date, :end_date, presence: true
+
+  def days_between
+    (end_date - start_date).to_i
+  end
 end
